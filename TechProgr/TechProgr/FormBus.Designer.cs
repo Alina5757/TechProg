5e9b9f597b0ef4c1fabd5c905aa707bbd084de0e
Alina5757 <91009972+Alina5757@users.noreply.github.com>
2021-09-23 19:57:43 +0400

    Add files via upload

---
+++ TechProgr/TechProgr/FormBus.Designer.cs
@@ -0,0 +1,140 @@
+﻿
+namespace TechProgr
+{
+	partial class FormBus
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormBus));
+			this.pictureBoxBus = new System.Windows.Forms.PictureBox();
+			this.buttonRight = new System.Windows.Forms.Button();
+			this.buttonDown = new System.Windows.Forms.Button();
+			this.buttonLeft = new System.Windows.Forms.Button();
+			this.buttonUp = new System.Windows.Forms.Button();
+			this.buttonCreate = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxBus)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBoxBus
+			// 
+			this.pictureBoxBus.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.pictureBoxBus.Location = new System.Drawing.Point(0, 0);
+			this.pictureBoxBus.Name = "pictureBoxBus";
+			this.pictureBoxBus.Size = new System.Drawing.Size(900, 500);
+			this.pictureBoxBus.TabIndex = 0;
+			this.pictureBoxBus.TabStop = false;
+			// 
+			// buttonRight
+			// 
+			this.buttonRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonRight.BackgroundImage")));
+			this.buttonRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.buttonRight.Location = new System.Drawing.Point(849, 415);
+			this.buttonRight.Name = "buttonRight";
+			this.buttonRight.Size = new System.Drawing.Size(40, 40);
+			this.buttonRight.TabIndex = 1;
+			this.buttonRight.UseVisualStyleBackColor = true;
+			this.buttonRight.Click += new System.EventHandler(this.buttomMove_Click);
+			// 
+			// buttonDown
+			// 
+			this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonDown.BackgroundImage")));
+			this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.buttonDown.Location = new System.Drawing.Point(803, 415);
+			this.buttonDown.Name = "buttonDown";
+			this.buttonDown.Size = new System.Drawing.Size(40, 40);
+			this.buttonDown.TabIndex = 2;
+			this.buttonDown.UseVisualStyleBackColor = true;
+			this.buttonDown.Click += new System.EventHandler(this.buttomMove_Click);
+			// 
+			// buttonLeft
+			// 
+			this.buttonLeft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonLeft.BackgroundImage")));
+			this.buttonLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.buttonLeft.Location = new System.Drawing.Point(757, 415);
+			this.buttonLeft.Name = "buttonLeft";
+			this.buttonLeft.Size = new System.Drawing.Size(40, 40);
+			this.buttonLeft.TabIndex = 3;
+			this.buttonLeft.UseVisualStyleBackColor = true;
+			this.buttonLeft.Click += new System.EventHandler(this.buttomMove_Click);
+			// 
+			// buttonUp
+			// 
+			this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonUp.BackgroundImage")));
+			this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+			this.buttonUp.Location = new System.Drawing.Point(803, 369);
+			this.buttonUp.Name = "buttonUp";
+			this.buttonUp.Size = new System.Drawing.Size(40, 40);
+			this.buttonUp.TabIndex = 4;
+			this.buttonUp.UseVisualStyleBackColor = true;
+			this.buttonUp.Click += new System.EventHandler(this.buttomMove_Click);
+			// 
+			// buttonCreate
+			// 
+			this.buttonCreate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.buttonCreate.BackColor = System.Drawing.SystemColors.Control;
+			this.buttonCreate.Location = new System.Drawing.Point(778, 461);
+			this.buttonCreate.Name = "buttonCreate";
+			this.buttonCreate.Size = new System.Drawing.Size(89, 29);
+			this.buttonCreate.TabIndex = 5;
+			this.buttonCreate.Text = "Create";
+			this.buttonCreate.UseVisualStyleBackColor = false;
+			this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+			// 
+			// FormBus
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(900, 500);
+			this.Controls.Add(this.buttonCreate);
+			this.Controls.Add(this.buttonUp);
+			this.Controls.Add(this.buttonLeft);
+			this.Controls.Add(this.buttonDown);
+			this.Controls.Add(this.buttonRight);
+			this.Controls.Add(this.pictureBoxBus);
+			this.Name = "FormBus";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "FormBus";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBoxBus)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBoxBus;
+		private System.Windows.Forms.Button buttonRight;
+		private System.Windows.Forms.Button buttonDown;
+		private System.Windows.Forms.Button buttonLeft;
+		private System.Windows.Forms.Button buttonUp;
+		private System.Windows.Forms.Button buttonCreate;
+	}
+}
+
---
+++ TechProgr/TechProgr/FormBus.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TechProgr
+{
+	public partial class FormBus : Form
+	{
+		private BusTwoFloor Bus;
+		private bool bus_exist = false;
+		public FormBus()
+		{
+			InitializeComponent();
+			Bus = new BusTwoFloor();
+		}
+		private void Draw() {
+			Bitmap bmp = new Bitmap(pictureBoxBus.Width, pictureBoxBus.Height);
+			Graphics gr = Graphics.FromImage(bmp);
+			Bus.DrowBus(gr);
+			pictureBoxBus.Image = bmp;
+		}
+		private void buttonCreate_Click(object sender, EventArgs e)
+		{
+			Random random = new Random();
+			Bus = new BusTwoFloor();
+			Bus.Init(random.Next(100, 200), random.Next(1000, 3000), true, true, Color.Red, Color.Yellow);
+			Bus.SetPosition(random.Next(200, 700), random.Next(200, 400));
+			bus_exist = true;
+			Draw();
+		}
+		private void buttomMove_Click(object sender, EventArgs e) {
+			string name = (sender as Button).Name;
+			if (bus_exist)
+			{
+				switch (name)
+				{
+					case "buttonUp":
+						Bus.MoveTransport(Direction.Up);
+						break;
+					case "buttonDown":
+						Bus.MoveTransport(Direction.Down);
+						break;
+					case "buttonLeft":
+						Bus.MoveTransport(Direction.Left);
+						break;
+					case "buttonRight":
+						Bus.MoveTransport(Direction.Right);
+						break;
+				}
+				Draw();
+			}
+		}
+	}
+}
